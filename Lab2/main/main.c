e3c07557827a21dffae53fd0357a14ab7d9dd88e
leexuaanbachs <114084888+leexuaanbachs@users.noreply.github.com>
2023-10-02 22:45:17 +0700

    Update main.c
    xóa timeout và 'else' trong for( ; ; )
    thêm một số comment

--- Lab2/main/main.c
+++ Lab2/main/main.c
@@ -18,12 +18,12 @@
 #define NORMAL_STATE 1
 
 int button1_flag = 0;
-int TimeOutForKeyPress =  500;
 int KeyReg3 = NORMAL_STATE;
 int KeyReg2 = NORMAL_STATE;
 int KeyReg1 = NORMAL_STATE;
 int KeyReg0 = NORMAL_STATE;
 
+// process when know that button is pressed.
 void subKeyProcess(){
 	button1_flag = 1;
     printf("ESP32\n");
@@ -36,6 +36,7 @@ void subKeyProcess(){
 	}
 }
 
+// cyclic task which print student identifier every second.
 void studentID1(void * p)
 {
     for (;;)
@@ -46,16 +47,19 @@ void studentID1(void * p)
     vTaskDelete(NULL);
 }
 
+
 void pollingButton(void * p)
 {
+	// set pin to be pullup input only
     esp_rom_gpio_pad_select_gpio ( BUTTON_PIN ) ;
     gpio_set_direction ( BUTTON_PIN , GPIO_MODE_INPUT ) ;
     gpio_set_pull_mode(BUTTON_PIN, GPIO_PULLUP_ONLY);
 
 
-
+// loop that receive signal from input pin above then give it to process function
     for (;;)
     {
+	    // debounce button signal
         KeyReg2 = KeyReg1;
         KeyReg1 = KeyReg0;
         KeyReg0 = gpio_get_level(BUTTON_PIN);
@@ -65,14 +69,8 @@ void pollingButton(void * p)
                 KeyReg3 = KeyReg2;
 
                 if (KeyReg3 == PRESSED_STATE){
-                    TimeOutForKeyPress = 200;
+			// when button is pressed
                     subKeyProcess();
-                }
-            } else
-            {
-                TimeOutForKeyPress --;
-                if (TimeOutForKeyPress == 0){
-                    KeyReg3 = NORMAL_STATE;
                 }
             }
         }
@@ -84,6 +82,8 @@ void pollingButton(void * p)
 
 void app_main(void)
 {
+	// first task create
     xTaskCreate(&studentID1, "ID1", 2048, NULL, 5, NULL);
+	// second task create
     xTaskCreate(&pollingButton, "ID2", 2048, NULL, 5, NULL);
 }
