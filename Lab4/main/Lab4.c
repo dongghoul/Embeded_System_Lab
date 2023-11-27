7d16a50d1fd1ff48048ce3b91baf5a31000a5a7a
haonguyenbzir2002 <78191012+haonguyenbzir2002@users.noreply.github.com>
2023-11-27 16:31:15 +0700

    Update Lab4

--- Lab4/main/Lab4.c
+++ Lab4/main/Lab4.c
@@ -5,7 +5,8 @@
 #include "freertos/FreeRTOSConfig.h"
 
 #define NUM_TASK 3 // Number of functional tasks
-#define MAX_REQUEST_ID NUM_TASK + 2
+#define MAX_REQUEST_ID (NUM_TASK + 2)
+#define ALL_CHECKED (0 | ((1 << NUM_TASK) - 1)) // Flag to check if all tasks have inspected a request
 
 // Queue handle for storing requests
 QueueHandle_t requestQueue;
@@ -14,7 +15,7 @@ QueueHandle_t requestQueue;
 typedef struct {
     int taskID;
     int data;
-    int counter;  // Counter to track how many tasks have inspected the request
+    uint8_t flags;  // flags to track tasks that have inspected the request
 } Request;
 
 // Reception task function
@@ -23,14 +24,14 @@ void receptionTask(void *pvParameters)
     while (1)
     {
         // Simulate receiving requests from somewhere
-        Request newRequest;
-        newRequest.taskID = rand() % (MAX_REQUEST_ID);
-        newRequest.data = rand() % 100;
-        newRequest.counter = NUM_TASK;
+        Request* newRequest = (Request*) malloc(sizeof(Request));
+        newRequest->taskID = rand() % (MAX_REQUEST_ID);
+        newRequest->data = rand() % 100;
+        newRequest->flags = 0;
 
         // Send the request to the queue
-        xQueueSend(requestQueue, &newRequest, portMAX_DELAY);
-        printf("Sended request {ID: %d, data: %d} to queue\n", newRequest.taskID, newRequest.data);
+        xQueueSend(requestQueue, (void *) &newRequest, portMAX_DELAY);
+        printf("Sended request {ID: %d, data: %d} to queue\n", newRequest->taskID, newRequest->data);
     
         // Simulate delay between requests
         vTaskDelay(pdMS_TO_TICKS(1000)); // Delay for 1 second
@@ -45,41 +46,41 @@ void functionalTask(void *pvParameters)
 
     while (1)
     {
-        // Request *receivedRequest = (Request*) malloc(sizeof(Request));
-        Request receivedRequest;
+        Request* receivedRequest = NULL;
 
         // Peek at the next item in the queue without removing it
-        if (xQueueReceive(requestQueue, &receivedRequest, portMAX_DELAY))
+        if (xQueuePeek(requestQueue, &receivedRequest, portMAX_DELAY))
         {
-            // Check if the request is for this task
-            if (receivedRequest.taskID == taskID)
-            {
-                // xQueueReceive(requestQueue, &receivedRequest, 0);
 
-                printf("Task %d received request {ID: %d, data: %d}\n", taskID, receivedRequest.taskID, receivedRequest.data);
+            // Check if the request is for this task
+            if (receivedRequest->taskID == taskID)
+            {
+                xQueueReceive(requestQueue, &receivedRequest, 0);
+
+                printf("Task %d received request {ID: %d, data: %d}\n", taskID, receivedRequest->taskID, receivedRequest->data);
                 // Handle the request here
+                free(receivedRequest);
+                receivedRequest = NULL;
             }
             else
             {
                 // Ignore request
-                printf("Task %d ignored request {ID: %d, data: %d}\n", taskID, receivedRequest.taskID, receivedRequest.data);
+                printf("Task %d ignored request {ID: %d, data: %d}\n", taskID, receivedRequest->taskID, receivedRequest->data);
 
-                // Decrement the counter
-                receivedRequest.counter--;
-                // printf("%d %d\n", receivedRequest.counter, receivedRequest.data);
+                // Signal the flag
+                receivedRequest->flags |= ((1 << taskID));
+                // printf("%d %d\n", receivedRequest->flags, ALL_CHECKED);
 
                 // Remove the request from the queue if all tasks have inspected it
-                // else send it back to the queue
-                if (receivedRequest.counter <= 0)
+                if (receivedRequest->flags == ALL_CHECKED)
                 {
-                    // xQueueReceive(requestQueue, receivedRequest, 0);
+                    xQueueReceive(requestQueue, &receivedRequest, 0);
                     // Raise an error and remove request
-                    printf("Task %d removed request {ID: %d, data: %d} from queue\n", taskID, receivedRequest.taskID, receivedRequest.data);
+                    printf("Task %d removed request {ID: %d, data: %d} from queue\n", taskID, receivedRequest->taskID, receivedRequest->data);
+                    free(receivedRequest);
+                    receivedRequest = NULL;
                 }
-                else 
-                {
-                    xQueueSendToFront(requestQueue, &receivedRequest, portMAX_DELAY);
-                }
+
                 vTaskDelay(1);
             }
         }
@@ -90,7 +91,7 @@ void functionalTask(void *pvParameters)
 void app_main()
 {
     // Create the request queue
-    requestQueue = xQueueCreate(10, sizeof(Request));
+    requestQueue = xQueueCreate(10, sizeof(Request*));
 
     // Create the reception task
     xTaskCreate(receptionTask, "ReceptionTask", 2048, NULL, 1, NULL);
